5e5676630f85c01a50f50c7eb4b078b94005a3f6
Krishnakumari <kyachamaneni@unomaha.edu>
2020-03-23 15:18:52 -0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@%65th6#j4!0s31$@#!-n*otgm=mxyaf=+$yi482%w6j^q^^f4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
