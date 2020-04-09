90f9d19f62a4ce2337c3cac53780b68732f6a092
Krishnakumari <kyachamaneni@unomaha.edu>
2020-04-09 15:50:08 -0500

    changes

--- orders/views.py
+++ orders/views.py
@@ -36,12 +36,7 @@ def order_create(request):
             toaddresses.append(request.user.email)
             send_mail('Order Placed Successfully', 'Hi '+order.last_name+'\n\nYour order of total $'+str(order.totalprice)+' has been successfully placed.\n\n Yours sincerely. \nEuropes Corner', request.user.email, toaddresses)
 
-            # launch asynchronous task
-            order_created.delay(order.id)
-            # set the order in the session
-            request.session['order_id'] = order.id
-            # redirect for payment
-            return redirect(reverse('payment:process'))
+
 
             cart.clear()
             return render(request,
--- shop/views.py
+++ shop/views.py
@@ -42,17 +42,8 @@ def product_detail(request, id, slug):
                        'searchterm': search_term})
 
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'productdetail.html', {'product': product})
-
-
-
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id,
-                                         slug=slug,
-                                         available=True)
     cart_product_form = CartAddProductForm()
-    return render(request,
-                  'productdetail.html',
-                  {'product': product,
-                   'cart_product_form': cart_product_form})
+    return render(request, 'productdetail.html', {'product': product,'cart_product_form': cart_product_form})
+
+
 
